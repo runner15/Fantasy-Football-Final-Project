cb552c72f6e3992795793e6c76e39991a80c639e
runner15 <therunner151@gmail.com>
2016-05-10 13:52:53 -0400

    Cannot get the rawData to get all the player data

--- FantasyFootballFinalProject.m
+++ FantasyFootballFinalProject.m
@@ -9,23 +9,25 @@ for n=1:17
     week(n,:)=webread(url);
 end
 %% Create better players array
-for j=1:length(allPlayers.players.player)
-    players(j).id=allPlayers.players.player{j,1}.id;
-    players(j).info.name=allPlayers.players.player{j,1}.name;
-    players(j).info.position=allPlayers.players.player{j,1}.position;
-    players(j).info.team=allPlayers.players.player{j,1}.team;
+for l=1:length(allPlayers.players.player)
+    players(l).id=allPlayers.players.player{l,1}.id;
+    players(l).info.name=allPlayers.players.player{l,1}.name;
+    players(l).info.position=allPlayers.players.player{l,1}.position;
+    players(l).info.team=allPlayers.players.player{l,1}.team;
 end
 %% Create raw roster data
 allRostersMat = zeros(19,12);
 for k=1:length(roster.rosters.franchise)
-    for i=1:length(roster.rosters.franchise(k).player)
-        allRostersMat(i,k) = str2double(roster.rosters.franchise(k).player(i,1).id);
+    for l=1:length(roster.rosters.franchise(k).player)
+        allRostersMat(l,k) = str2double(roster.rosters.franchise(k).player(l,1).id);
         rawData(k).franchise = roster.rosters.franchise(k);
         rawData(k).franchise.name = franchise.league.franchises.franchise{k,1}.name;
-        index = find(strcmp({rawData(k).franchise.player(i).id}, {players.id})==1);
+        index = find(strcmp({rawData(k).franchise.player(l).id}, {players.id})==1);
         if (index ~= 0)
-            indexraw(i,k) = index;
-        end
-    end
+            indexraw(l,k) = index;
+            %rawData(k).franchise.player(l).position=players(index).info.position;
+            %rawData(k).franchise.player(l).name=players(index).info.name;
+            rawData(k).franchise.player(l).team=players(index).info.team;
+        end  
+    end   
 end
-rawData(1).franchise.player(2).id
