ccffb214efb363aec619d8c8dc3c364930a227e9
runner15 <therunner151@gmail.com>
2016-05-10 12:03:03 -0400

    Create a better players and roster structures

--- FantasyFootballFinalProject.m
+++ FantasyFootballFinalProject.m
@@ -1,21 +1,26 @@
+%% Initialize variables
 clear,clc
 allPlayers=webread( 'http://www03.myfantasyleague.com/2016/export?TYPE=players&L=&W=&JSON=1');
 roster=webread('http://www60.myfantasyleague.com/2016/export?TYPE=rosters&L=76845&W=&JSON=1');
+franchise=webread('http://www60.myfantasyleague.com/2016/export?TYPE=league&L=76845&W=&JSON=1');
 for n=1:17
     url='http://www59.myfantasyleague.com/2015/export?TYPE=playerScores&JSON=1&L=35465&W=';
     url=strcat(url,num2str(n));
     week(n,:)=webread(url);
 end
-%{
-fname = 'players.json';
-fid = fopen(fname);
-raw = fread(fid,inf);
-allPlayers = char(raw');
-fclose(fid);
-data = JSON.parse(allPlayers);
-%}
-
-for n = 1:length(allPlayers.players.player)
-    fprintf('Players name: %s\n', ...
-        allPlayers.players.player{n,1}.name)
+%% Create better players array
+for j=1:length(allPlayers.players.player)
+    players(j).id=allPlayers.players.player{j,1}.id;
+    players(j).name=allPlayers.players.player{j,1}.name;
+    players(j).position=allPlayers.players.player{j,1}.position;
+    players(j).team=allPlayers.players.player{j,1}.team;
+end
+%% Create raw roster data
+allRostersMat = zeros(19,12);
+for k=1:length(roster.rosters.franchise)
+    for i=1:length(roster.rosters.franchise(k).player)
+        allRostersMat(i,k) = str2double(roster.rosters.franchise(k).player(i,1).id);
+        rawData(k).franchise = roster.rosters.franchise(k);
+        rawData(k).franchise.name = franchise.league.franchises.franchise{k,1}.name;
+    end
 end
